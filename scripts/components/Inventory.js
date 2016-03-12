a36f25bedefdc69753a8e82482bcc045dc1c8d3d
chao-xian <dev@kelvingan.co.uk>
2016-03-12 16:43:42 +0000

    Add auth. FINIS

--- scripts/components/Inventory.js
+++ scripts/components/Inventory.js
@@ -5,9 +5,79 @@
 import React from 'react';
 import AddFishForm from './AddFishForm';
 import autobind from 'autobind-decorator';
+import Firebase from 'firebase';
+const ref = new Firebase('https://blazing-inferno-2155.firebaseio.com/')
 
 @autobind
 export class Inventory extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      uid: ''
+    }
+  }
+
+  authenticate(provider) {
+    console.log("Trying to authenticate with " + provider);
+    ref.authWithOAuthPopup(provider, this.authHandler);
+  }
+
+  componentWillMount() {
+    console.log("Check local storage if logged in");
+    var token = localStorage.getItem('token');
+    if(token) {
+      ref.authWithCustomToken(token, this.authHandler);
+    }
+  }
+
+  logout() {
+    ref.unauth();
+    localStorage.removeItem('token');
+    this.setState({
+      uid: null
+    });
+  }
+
+  authHandler(err, authData) {
+    if (err) {
+      console.err(err);
+      return;
+    }
+
+    // save login token into browser
+    localStorage.setItem('token', authData.token);
+
+    const storeRef = ref.child(this.props.params.storeId);
+    storeRef.on('value', (snapshot)=>{
+      var data = snapshot.val() || {};
+
+      // claim store if no owner
+      if (!data.owner) {
+        storeRef.set({
+          owner: authData.uid
+        });
+      }
+
+      // update state to reflect current store and user
+      this.setState({
+        uid: authData.uid,
+        owner: data.owner || authData.uid
+      })
+    });
+  }
+
+  renderLogin() {
+    return (
+      <nav className="login">
+        <h2>Inventory</h2>
+        <p>Sign in to manage your store's inventory</p>
+        <button className="github" onClick={this.authenticate.bind(this, 'github')}>Log in with GitHub</button>
+      </nav>
+    )
+  }
+
   renderInventory(key) {
     var linkState = this.props.linkState;
     return (
@@ -26,9 +96,24 @@ export class Inventory extends React.Component {
   }
 
   render() {
+    let logoutButton = <button onClick={this.logout}>Log out!</button>
+
+    if (!this.state.uid) {
+      return (
+        <div>{ this.renderLogin() }</div>
+      )
+    }
+
+    if (this.state.uid !== this.state.owner) {
+      return(
+        <div><p>Sorry you aren't the owner of this store</p></div>
+      )
+    }
+
     return (
       <div>
         <h2>Inventory</h2>
+        { logoutButton }
 
         {/* Grab all the keys for fishes and over each call renderInventory */}
         {Object.keys(this.props.fishes).map(this.renderInventory)}
